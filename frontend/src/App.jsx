fc3347e58be4835d9d52f4bfbf72ae5c10932737
tamy290 <lau.tami5@gmail.com>
2024-10-24 23:29:26 -0300

    cambios en header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Registro from './auth/Registro';
-import Login from './auth/login';
+import Login from './auth/Login';
+import Header from './shared/Header';
+
 const App = () => {
     return (
         <Router>
+            <Header />
             <div className="container mt-4">
                 <Routes>
-                    <Route path="/" element={<Registro />} />
+                    <Route path="/" element={<h2>Bienvenido a la Plataforma de Bienestar Corporativo</h2>} />
+                    <Route path="/registro" element={<Registro />} />
                     <Route path="/login" element={<Login />} />
                 </Routes>
             </div>
@@ -16,6 +20,8 @@ const App = () => {
     );
 };
 
-export default App;
+export default App;
+
+
 
 

b28a5eef5ec84b9a10fe6995706458982aedb523
Leila <vleila723@gmail.com>
2024-10-26 11:50:45 -0300

    modelado de la BD y funcionamiento de registro

---
+++ backend/src/controllers/session.controller.js
@@ -0,0 +1,54 @@
+import User from "../models/user.model.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const login = async (req, res) => {
+    //Validemos el usuario y la contraseña
+    try {
+        const { email, contraseña } = req.body;
+        const user = await User.findOne({ email }); //Buscamos el usuario por email
+        if (!user) {    //Si no existe el usuario
+            res.status(401).json({
+                errors: {
+                    email: {
+                        message: "¡Usuario no encontrado!"
+                    }
+                }
+            });
+            return;
+        }
+
+        const passwordMatch = await bcrypt.compare(contraseña, user.contraseña); //Comparamos la contraseña que llega con la encriptada
+        if (!passwordMatch) {   //Si no coincide
+            res.status(401).json({
+                errors: {
+                    password: {
+                        message: "¡Contraseña incorrecta!"
+                    }
+                }
+            });
+            return;
+        }
+        //Generamos el token
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+        res.status(200)     //Enviamos el estado 200
+            .cookie("userToken", token, { httpOnly: true }) //Enviamos el token en una cookie
+            .json({ token, user: { nombre: user.nombre } });   //Enviamos el token en la respuesta
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+
+};
+
+const logout = async (req, res) => {
+    res.status(200).clearCookie("userToken").json({});
+};
+
+const session = async (req, res) => {
+    res.status(200).json(req.user);
+};
+
+
+export default { login, logout, session };
--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,13 +1,14 @@
-// faltaria importacion de models
+import User from '../models/user.model.js';
 
 const create = async (req, res) => {
     try {
         const data = req.body;
+        console.log(data);
         const newElement = await User.create(data);
         res.status(201).json(newElement);
     } catch (error) {
         console.error(error);
-        if (error.code === 11000) {  // Clave duplicada
+        if (error.code === 11000) {  //Clave duplicada
             res.status(400).json({ message: 'El correo electrónico ya está registrado' });
         } else {
             res.status(400).json({ message: error.message });

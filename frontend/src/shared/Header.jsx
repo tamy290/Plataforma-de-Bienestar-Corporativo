9c5ce16a2228402eec88263dbec55de2d5fa4369
tamy290 <lau.tami5@gmail.com>
2024-10-28 23:41:22 -0300

    actualizar

--- frontend/src/shared/Header.jsx
+++ frontend/src/shared/Header.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Navbar, Nav, Button } from 'react-bootstrap';
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    // Verifica si hay un token en el almacenamiento local
+    const isAuthenticated = !!localStorage.getItem('token');
+
+    const handleLogout = () => {
+        // Elimina el token para cerrar la sesión
+        localStorage.removeItem('token');
+        // Redirige al usuario a la página de inicio de sesión
+        navigate('/login');
+    };
+
     return (
         <Navbar bg="light" expand="lg">
             <Navbar.Brand 
                 style={{ 
                     fontFamily: 'Montserrat, sans-serif', 
                     fontWeight: 'bold', 
-                    backgroundColor: '#F5F5F5',  // Fondo suave
-                    padding: '5px 10px', // Espaciado interno para mejor apariencia
-                    borderRadius: '5px'  // Bordes redondeados para un estilo más amigable
+                    backgroundColor: '#F5F5F5',
+                    padding: '5px 10px',
+                    borderRadius: '5px'
                 }}
             >
                 <span style={{ color: '#4CAF50' }}>Vita</span>
@@ -20,12 +32,20 @@ const Header = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
-                    <Link to="/login">
-                        <Button variant="outline-primary" className="me-2">Iniciar Sesión</Button>
-                    </Link>
-                    <Link to="/registro">
-                        <Button variant="outline-primary">Registrarse</Button>
-                    </Link>
+                    {isAuthenticated ? (
+                        <Button variant="outline-danger" onClick={handleLogout}>
+                            Cerrar Sesión
+                        </Button>
+                    ) : (
+                        <>
+                            <Link to="/login">
+                                <Button variant="outline-primary" className="me-2">Iniciar Sesión</Button>
+                            </Link>
+                            <Link to="/registro">
+                                <Button variant="outline-primary">Registrarse</Button>
+                            </Link>
+                        </>
+                    )}
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
@@ -33,5 +53,6 @@ const Header = () => {
 };
 
 export default Header;
+
 
 
